ac509662f087c6f6af9000e907c02487c68115ed
Longdy19 <ouklongdy@gmail.com>
2025-09-29 10:09:28 +0700

    Initial -Strategic Excellence Demo Website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Strategic Excellence Demo</h1>
+        <p>Clean starter version</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
